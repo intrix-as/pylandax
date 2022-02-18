7a6c4119242cb95a4ec71111fe732edded0774ca
David Skoland <davidskoland@gmail.com>
2022-02-18 21:08:34 +0100

    Add support for getting more than 1000 records

--- pylandax/client.py
+++ pylandax/client.py
@@ -1,6 +1,7 @@
 import pyodata
 import requests
 import json
+import copy
 from pyodata.v2.model import PolicyFatal, PolicyWarning, PolicyIgnore, ParserError, Config
 
 
@@ -51,7 +52,7 @@ class Client:
 
 		self.odata_client = pyodata.Client(self.api_url, session, config=odata_config)
 
-	def get_data(self, data_model: str) -> [{}]:
+	def get_data(self, data_model: str) -> {}:
 		if self.format == 'xml':
 			# WIP: the pyodata library from SAP doesn't parse ATOM correctly
 			# See https://github.com/SAP/python-pyodata/issues/202
@@ -60,13 +61,53 @@ class Client:
 			data = getattr(self.odata_client.entity_sets, data_model)
 			print(data.get_entities().execute())
 		elif self.format == 'json':
-			url = f'{self.api_url}{data_model}?$format=json'
+			initial_url = url = f'{self.api_url}{data_model}?$format=json&$top=1000'
+			metakey = 'Number'
+			values = {}
+			data = self.request_data(initial_url)
 
-			response = requests.get(url, headers=self.headers)
+			self.add_list_to_dict(values, data, metakey)
 
-			return response.json()['d']['results']
+			count = len(data)
+			if count != 1000:
+				return values
+
+			# If count is 1000, there is a chance that there are more than 1000 records
+			# so we need to make additional requests until we get less than 1000
+			thousands = 0
+			while count == 1000:
+				thousands = thousands + 1
+				new_url = initial_url + '&$skip=' + str(thousands * 1000)
+				new_data = self.request_data(new_url)
+				self.add_list_to_dict(values, new_data, metakey)
+				count = len(new_data)
+
+			return values
 		else:
 			print('Error: Unknown format: ' + self.format)
+
+	def post_data(self, data_model: str, data: dict):
+		url = self.api_url + data_model
+		headers = copy.deepcopy(self.headers)
+		headers['Content-Type'] = 'application/json'
+
+		response = requests.post(url, json=data, headers=headers)
+		return response
+
+	# Helper function for get_data
+	def request_data(self, url: str) -> []:
+		response = requests.get(url, headers=self.headers)
+		results = response.json()['d']['results']
+		return results
+
+	# Modifies the given dict_in with the given list_in using the metakey
+	@staticmethod
+	def add_list_to_dict(dict_in: {}, list_in: [{}], metakey: str):
+		for record in list_in:
+			key = record[metakey]
+			if key in dict_in:
+				print(f'Warning: {key} exists in values, overwriting')
+			dict_in[key] = record
 
 	# Contact the remote server for an OAuth token
 	def oauth_from_server(self):
