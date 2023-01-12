3942112dfd31f5fc4f59d5195c98438f69b39540
David Skoland <davidskoland@gmail.com>
2023-01-12 13:52:19 +0100

    Use spaces instead of tabs

--- pylandax/client.py
+++ pylandax/client.py
@@ -10,133 +10,134 @@ class LandaxAuthException(Exception):
 
 
 class Client:
-	def __init__(self, url: str, credentials: dict):
-		self.required_credentials = [
-			'username', 'password',
-			'client_id', 'client_secret'
-		]
+    def __init__(self, url: str, credentials: dict):
+        self.required_credentials = [
+            'username', 'password',
+            'client_id', 'client_secret'
+        ]
 
-		for key, value in credentials.items():
-			setattr(self, key, value)
+        for key, value in credentials.items():
+            setattr(self, key, value)
 
-		for attr in self.required_credentials:
-			if not hasattr(self, attr):
-				print(f'Error: credential field is required: {attr}')
-				return
+        for attr in self.required_credentials:
+            if not hasattr(self, attr):
+                print(f'Error: credential field is required: {attr}')
+                return
 
-		self.base_url = f'https://{url}/'
-		self.api_url = self.base_url + 'api/v20/'
-		self.headers = {}
+        self.base_url = f'https://{url}/'
+        self.api_url = self.base_url + 'api/v20/'
+        self.headers = {}
 
-		self.oauth_token = self.get_oauth_token()
+        self.oauth_token = self.get_oauth_token()
 
-		self.headers['Authorization'] = 'Bearer ' + self.oauth_token
+        self.headers['Authorization'] = 'Bearer ' + self.oauth_token
 
-	# Returns a record with the given data_id (Id in in landax)
-	def get_single_data(self, data_model: str, data_id: int):
-		url = f'{self.api_url}{data_model}({str(data_id)})?$format=json'
-		response = requests.get(url, headers=self.headers)
-		if response.status_code == 404:
-			return None
+    # Returns a record with the given data_id (Id in in landax)
+    def get_single_data(self, data_model: str, data_id: int):
+        url = f'{self.api_url}{data_model}({str(data_id)})?$format=json'
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 404:
+            return None
 
-		data = response.json()
-		return data
+        data = response.json()
+        return data
 
-	# Returns all records of the given data model
-	def get_all_data(self, data_model: str) -> [{}]:
-		initial_url = f'{self.api_url}{data_model}?$format=json&$top=1000'
-		data = self.request_data(initial_url)
-		count = len(data)
-		if count != 1000:
-			return data
+    # Returns all records of the given data model
+    def get_all_data(self, data_model: str) -> [{}]:
+        initial_url = f'{self.api_url}{data_model}?$format=json&$top=1000'
+        data = self.request_data(initial_url)
+        count = len(data)
+        if count != 1000:
+            return data
 
-		# If count is 1000, there is a chance that there are more than 1000 records
-		# since Landax only returns 1000 records max at a time
-		# so we need to make additional requests until we get less than 1000
-		thousands = 0
-		while count == 1000:
-			thousands = thousands + 1
-			new_url = initial_url + '&$skip=' + str(thousands * 1000)
-			new_data = self.request_data(new_url)
-			data = data + new_data
-			count = len(new_data)
+        # If count is 1000, there is a chance that there are more than 1000 records
+        # since Landax only returns 1000 records max at a time
+        # so we need to make additional requests until we get less than 1000
+        thousands = 0
+        while count == 1000:
+            thousands = thousands + 1
+            new_url = initial_url + '&$skip=' + str(thousands * 1000)
+            new_data = self.request_data(new_url)
+            data = data + new_data
+            count = len(new_data)
 
-		return data
+        return data
 
-	def post_data(self, data_model: str, data: dict):
-		url = self.api_url + data_model
-		headers = copy.deepcopy(self.headers)
-		headers['Content-Type'] = 'application/json'
+    def post_data(self, data_model: str, data: dict):
+        url = self.api_url + data_model
+        headers = copy.deepcopy(self.headers)
+        headers['Content-Type'] = 'application/json'
 
-		response = requests.post(url, json=data, headers=headers)
-		return response
+        response = requests.post(url, json=data, headers=headers)
+        return response
 
-	# Patches the given data model with the given id with data
-	def patch_data(self, data_model: str, key: int, data: dict):
-		url = f'{self.api_url}{data_model}({str(key)})'
-		headers = copy.deepcopy(self.headers)
-		headers['Content-Type'] = 'application/json'
+    # Patches the given data model with the given id with data
+    def patch_data(self, data_model: str, key: int, data: dict):
+        url = f'{self.api_url}{data_model}({str(key)})'
+        headers = copy.deepcopy(self.headers)
+        headers['Content-Type'] = 'application/json'
 
-		response = requests.patch(url, json=data, headers=headers)
-		return response
+        response = requests.patch(url, json=data, headers=headers)
+        return response
 
-	# Deletes data with the given key
-	def delete_data(self, data_model: str, key: str):
-		url = f'{self.api_url}{data_model}({key})?$format=json'
-		response = requests.delete(url, headers=self.headers)
-		if response.status_code == 404:
-			return None
-		return response
+    # Deletes data with the given key
+    def delete_data(self, data_model: str, key: str):
+        url = f'{self.api_url}{data_model}({key})?$format=json'
+        response = requests.delete(url, headers=self.headers)
+        if response.status_code == 404:
+            return None
+        return response
 
-	# Helper function for get_data
-	def request_data(self, url: str) -> []:
-		response = requests.get(url, headers=self.headers)
-		results = response.json()['value']
-		return results
+    # Helper function for get_data
+    def request_data(self, url: str) -> []:
+        response = requests.get(url, headers=self.headers)
+        results = response.json()['value']
+        return results
 
-	def upload_document(self, file: pathlib.Path, folder_id: int):
-		url = self.api_url + 'Documents/CreateDocument'
-		document = json.dumps({'FolderId': folder_id})
+    def upload_document(self, file: pathlib.Path, folder_id: int):
+        url = self.api_url + 'Documents/CreateDocument'
+        document = json.dumps({'FolderId': folder_id})
 
-		files = {
-			'document': (None, document),
-			'fileData': (str(file.name), open(file, 'rb'))
-		}
+        files = {
+            'document': (None, document),
+            'fileData': (str(file.name), open(file, 'rb'))
+        }
 
-		response = requests.post(url, files=files, headers=self.headers)
+        response = requests.post(url, files=files, headers=self.headers)
 
-		return response
+        return response
 
-	# Creates a dict given the list of dicts list_in using the metakey
-	@staticmethod
-	def list_to_dict(list_in: [{}], metakey: str):
-		return_dict = {}
+    # Creates a dict given the list of dicts list_in using the metakey
+    @staticmethod
+    def list_to_dict(list_in: [{}], metakey: str):
+        return_dict = {}
 
-		for record in list_in:
-			key = record[metakey]
-			if key in return_dict:
-				print(f'Warning: {key} already present, overwriting')
-			return_dict[key] = record
+        for record in list_in:
+            key = record[metakey]
+            if key in return_dict:
+                print(f'Warning: {key} already present, overwriting')
+            return_dict[key] = record
 
-		return return_dict
+        return return_dict
 
-	def get_oauth_token(self):
-		url = self.base_url + 'authenticate/token?grant_type=password'
+    def get_oauth_token(self):
+        url = self.base_url + 'authenticate/token?grant_type=password'
 
-		post_body = {
-			'client_id': self.client_id,
-			'client_secret': self.client_secret,
-			'username': self.username,
-			'password': self.password
-		}
+        post_body = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'username': self.username,
+            'password': self.password
+        }
 
-		result = requests.post(url, json=post_body)
-		if result.status_code != 200:
-			raise LandaxAuthException('Landax returned non-200 response when getting OAuth token. Body: ' + str(result.content))
+        result = requests.post(url, json=post_body)
+        if result.status_code != 200:
+            raise LandaxAuthException(
+                'Landax returned non-200 response when getting OAuth token. Body: ' + str(result.content))
 
-		response_data = result.json()
+        response_data = result.json()
 
-		if 'access_token' not in response_data:
-			raise LandaxAuthException('Landax response was non-json. Body: ' + str(result.content))
+        if 'access_token' not in response_data:
+            raise LandaxAuthException('Landax response was non-json. Body: ' + str(result.content))
 
-		return response_data['access_token']
+        return response_data['access_token']
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,17 +4,16 @@ from pathlib import Path
 
 import pylandax
 
-
 script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
 def test_basic():
-	confpath = Path(script_dir, 'mock_config.json')
-	with open(confpath) as file:
-		conf = json.loads(file.read())['landax']
+    confpath = Path(script_dir, 'mock_config.json')
+    with open(confpath) as file:
+        conf = json.loads(file.read())['landax']
 
-	try:
-		client = pylandax.Client(conf['url'], conf['credentials'])
-	# Since the URL is bogus, this is what we expect
-	except pylandax.LandaxAuthException:
-		pass
+    try:
+        client = pylandax.Client(conf['url'], conf['credentials'])
+    # Since the URL is bogus, this is what we expect
+    except pylandax.LandaxAuthException:
+        pass
